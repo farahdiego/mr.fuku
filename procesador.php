c120f095ac64cc6967716a20662268b87631e54f
farahdiego <farahdiego@users.noreply.github.com>
2021-02-17 22:48:03 -0300

    procesador

--- procesador.php
+++ procesador.php
@@ -2,39 +2,29 @@
 // Formulario php comentado. Los items en naranja pueden eliminarse
 // y son explicaciones de cada paso.
 
-// declaración de variables globales.
-$nombreyapellido=$_GET['nombreyapellido'];
-$telefono=$_GET['telefono'];
+// declaraciÃ³n de variables globales.
+$nombre=$_GET['nombre'] ?: '(sin nombre)';
 $correo=$_GET['correo'];
-$nombreyapellidodelchico=$_GET['nombreyapellidodelchico'];
-$edad=$_GET['edad'];
-$fechadenacimiento=$_GET['fechadenacimiento'];
-$horario=$_GET['horario'];
 $empresa=$_GET['empresa'];
 $consulta=$_GET['consulta'];
 
-//Aquí se coloca el destinatario del mail (entre comillas dobles)
-mail("diegofarah@outlook.com",
+//AquÃ­ se coloca el destinatario del mail (entre comillas dobles)
+mail("info@teslamd.com",
 
 // Aqui va el asunto del mail (entre comillas dobles)
-"Consulta de Tesla MD",
+"Consulta de $nombre para Tesla MD",
 
 // Aqui van cada una de las variables (campos del formulario entre comillas dobles)
-// en el órden en que se reciben en el cuerpo del mail
-"\nnombreyapellido:$nombreyapellido
-\ntelefono:$telefono
-\ncorreo:$correo
-\nnombreyapellidodelchico:$nombreyapellidodelchico
-\nedad:$edad
-\nfechadenacimiento:$fechadenacimiento
-\nhorario:$horario
-\nempresa:$empresa
-\nconsulta:$consulta",
+// en el Ã³rden en que se reciben en el cuerpo del mail
+"\nNombre: $nombre
+\nCorreo: $correo
+\nEmpresa: $empresa
+\nConsulta: $consulta",
 
 // Aqui va el Remitente del mail
-"From: diegofarah@outlook.com");
+"From: consulta_web@teslamd.com");
 
-// Aqui va la url del archivo de respuesta que indica al usuario que el formulario fué enviado
+// Aqui va la url del archivo de respuesta que indica al usuario que el formulario fuÃ© enviado
 // El comando header es el equivalente a link de html
 header("Location: gracias.html");
 ?>
